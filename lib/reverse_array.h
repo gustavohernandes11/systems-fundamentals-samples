6ca398b3f050d39b091e4939f2b2f4d9bfe2f257
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-01-29 21:16:54 -0300

    feat: solve problem 2.10: reverse_array

--- lib/reverse_array.h
+++ lib/reverse_array.h
@@ -4,5 +4,9 @@ void reverse_array(int a[], int cnt)
 {
     int first, last;
     for (first = 0, last = cnt - 1; first <= last; first++, last--)
+    {
+        if (last == first)
+            return;
         inplace_swap(&a[first], &a[last]);
+    }
 }
